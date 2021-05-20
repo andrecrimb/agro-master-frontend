55596c85c44517b2aab437091d0d56fd502ab5ac
Andre Rosa <ar@gsmk.de>
2021-05-20 15:56:04 +0200

    ➕ Remove moment and add dayjs to handle dates

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
-import moment from 'moment'
+import dayjs from 'dayjs'
 
 export const formatDate = (date: any, format: string = 'DD/MM/YYYY HH:mm'): string => {
   if (date === 'undefined' || !date) {
     return ''
   }
-  return moment(date).format(format)
+  return dayjs(date).format(format)
 }
